8026ad7bbadb0b7fdc1ddf2b9f1c314c9ec34f65
Waseem Irfan <waseemirfan2019@gmail.com>
2025-04-15 23:23:24 +0500

    playing with mutex_Semaphore

--- main/main.c
+++ main/main.c
@@ -1,20 +1,60 @@
 #include <stdio.h>
-#include "pid_control.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 
-pid_ctrl_block_handle_t pid;
-pcnt_unit_handle_t pcnt;
-bdc_motor_handle_t motor;
+SemaphoreHandle_t mutex;
+
+void I2C_Bus(char * msg){
+    printf(msg);
+}
+
+void Reading_MPU6050(void * params){
+    while(1){
+        printf("Reading MPU6050\n");
+    if(xSemaphoreTake(mutex, pdMS_TO_TICKS(100))){
+        I2C_Bus("X = 88\n");
+        xSemaphoreGive(mutex);
+    }
+    else{
+        printf("MPU6050 timed out\n");
+    } 
+    vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void Reading_Pulses(void * params){
+    while(1){
+        printf("Reading Pulses\n");
+    if(xSemaphoreTake(mutex, pdMS_TO_TICKS(100))){
+        I2C_Bus("Pulses = 2222\n");
+        xSemaphoreGive(mutex);
+    }
+    else{
+        printf("Pulses timed out\n");
+    } 
+    vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void MQTT_Publisher(void* params){
+    while(1){
+        printf("Publishing MPU6050 & Pulses\n");
+            if(xSemaphoreTake(mutex, pdMS_TO_TICKS(200))){
+                I2C_Bus("published X=88\n");
+                I2C_Bus("Published Pulses = 2222\n");
+        }
+        else{
+                printf("MQTT_Publisher timed out\n");
+        } 
+        xSemaphoreGive(mutex);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 void app_main(void){
-    pcnt = setup_pcnt_encoder(GPIO_NUM_5, GPIO_NUM_18, 32767, -32768);
-
-    // 2. Initialize motor
-    motor = start_motor(GPIO_NUM_19, GPIO_NUM_21, 25000); // 1kHz PWM
-
-    // 3. Initialize PID controller (already shown above)
-    ESP_ERROR_CHECK(pid_cfg_init(1.1f, 0.33f, 0.02f));
-    // 4. Create control task
-    xTaskCreate(control_loop_task, "pid_control", 4096, NULL, 5, NULL);
+    mutex = xSemaphoreCreateMutex();
+    xTaskCreatePinnedToCore(Reading_MPU6050,"Reading MPU", 1024*2, NULL,2,NULL,1);
+    xTaskCreatePinnedToCore(Reading_Pulses,"Reading Pulses", 1024*2, NULL,2,NULL,1);
+    xTaskCreatePinnedToCore(MQTT_Publisher,"Publisher", 1024*2, NULL,2,NULL,0);
 }
